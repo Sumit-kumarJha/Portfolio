900b5a705df4b453dc6f0ace6f74d77a20ce6e8d
Sumit Kumar Jha <sumitkjha.work@example.com>
2026-09-20 03:45:46 +0530

    fix: update LeetCode profile URL to _sumit_jha_

--- src/app/services/portfolio-data.service.ts
+++ src/app/services/portfolio-data.service.ts
@@ -29,7 +29,7 @@ export class PortfolioDataService {
     socialLinks: {
       github: 'https://github.com/Sumit-kumarJha',
       linkedin: 'https://linkedin.com/in/sumitkjha',
-      leetcode: 'https://leetcode.com/u/sumitkjha',
+      leetcode: 'https://leetcode.com/u/_sumit_jha_/',
       codechef: 'https://www.codechef.com/users/sumitkjha',
       codeforces: 'https://codeforces.com/profile/Online_Engineer',
     },
